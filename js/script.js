3a9122064bcb5b4661dc5d6124c07bc4b006692f
Ivan Bonnot <ivanbonnot@gmail.com>
2022-08-18 18:39:34 -0300

    agrega nota a html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,107 @@
-const titulo = document.querySelector('#titulo')
-const detalles = document.querySelector('#detalles')
+//const { DateTime } = require("luxon");
+
+import { DateTime } from "luxon";
+
+
+const tituloInput = document.querySelector('#titulo')
+const fechaInput = document.querySelector('#fecha')
+const horaInput = document.querySelector('#hora')
+const detallesInput = document.querySelector('#detalles')
+
+const contenedorNotas = document.querySelector('#notas');
+
+const crearNota = document.querySelector('#crearNota')
+
+let notas = []
+
+//console.log(DateTime.now().setZone('America/New_York').minus({weeks:1}).endOf('day').toISO())
+console.log(DateTime.now().ts())
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    tituloInput.addEventListener('change', datosNota);
+    fechaInput.addEventListener('change', datosNota);
+    horaInput.addEventListener('change', datosNota);
+    detallesInput.addEventListener('change', datosNota);
+    crearNota.addEventListener('click', agregarNota)
+}
+
+const notaObj = {
+    titulo: '',
+    creadaPor: '',
+    fecha: '',
+    hora:'',
+    detalles: ''
+}
+
+function datosNota(e) {
+    //  console.log(e.target.name) // Obtener el Input
+     notaObj[e.target.name] = e.target.value;
+     console.log(notaObj)
+}
+
+
+function agregarNota(e) { 
+    e.preventDefault();
+    limpiarHTML();
+    
+    notas = [...notas, notaObj]
+    console.log(notas)
+
+    notas.forEach(nota => {
+        console.log('entra')
+        const {titulo, creadaPor, fecha, hora, detalles, id } = nota;
+
+        const divNota = document.createElement('div');
+        divNota.classList.add('cita', 'p-3');
+        divNota.dataset.id = id;
+
+        // scRIPTING DE LOS ELEMENTOS...
+        const tituloParrafo = document.createElement('h2');
+        tituloParrafo.classList.add('card-title', 'font-weight-bolder');
+        tituloParrafo.innerHTML = `${titulo}`;
+
+        const creadaPorParrafo = document.createElement('p');
+        creadaPorParrafo.innerHTML = `<span class="font-weight-bolder">Creada por: </span> ${creadaPor}`;
+
+        const fechaParrafo = document.createElement('p');
+        fechaParrafo.innerHTML = `<span class="font-weight-bolder">Fecha: </span> ${fecha}`;
+
+        const horaParrafo = document.createElement('p');
+        horaParrafo.innerHTML = `<span class="font-weight-bolder">Hora: </span> ${hora}`;
+
+        const detallesParrafo = document.createElement('p');
+        detallesParrafo.innerHTML = `<span class="font-weight-bolder">Detalles: </span> ${detalles}`;
+
+        // Agregar un botón de eliminar...
+        const btnEliminar = document.createElement('button');
+        //btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+        btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+        btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+        // Añade un botón de editar...
+        const btnEditar = document.createElement('button');
+        //btnEditar.onclick = () => cargarEdicion(cita);
+
+        btnEditar.classList.add('btn', 'btn-info');
+        btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+        // Agregar al HTML
+        divNota.appendChild(tituloParrafo);
+        divNota.appendChild(creadaPorParrafo);
+        divNota.appendChild(fechaParrafo);
+        divNota.appendChild(horaParrafo);
+        divNota.appendChild(detallesParrafo);
+        divNota.appendChild(btnEliminar)
+        divNota.appendChild(btnEditar)
+
+        contenedorNotas.appendChild(divNota);
+    });    
+}
+
+function limpiarHTML() {
+    while(contenedorNotas.firstChild) {
+        contenedorNotas.removeChild(contenedorNotas.firstChild);
+    }
+}
